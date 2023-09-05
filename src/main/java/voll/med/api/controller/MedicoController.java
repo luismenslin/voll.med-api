db2c16efbb9746a6f6af49837fb39eaa482f5fbe
Fábio Menslin <luis.fmenslinx@gmail.com>
2023-09-05 13:30:03 -0300

    colocando validações

--- src/main/java/voll/med/api/controller/MedicoController.java
+++ src/main/java/voll/med/api/controller/MedicoController.java
@@ -1,17 +1,26 @@
 package voll.med.api.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import voll.med.api.medico.DadosCadastroMedico;
+import voll.med.api.medico.Medico;
+import voll.med.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
     }
 }
